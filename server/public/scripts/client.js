e3c455b464e54077e46914093bd9eae992745e7d
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-08-05 14:09:59 -0500

    GET on client and server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,8 @@ $( document ).ready( function(){
   // Establish Click Listeners
   setupClickListeners()
   // load existing koalas on page load
-  
+  getKoalas();
+
   console.log('our values', getValues());
 }); // end doc ready
 
@@ -41,8 +42,34 @@ function getValues() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    type: 'GET',
+    url: '/koalas'
+  }).then(function(response){
+    set(response);
+    console.log('response', response);
+  }).catch(function(error){
+    console.log('error in client', error);
+  })
 } // end getKoalas
+
+function set(koalas){
+  $('#viewKoalas').empty();
+
+  for (let i = 0; i < koalas.length; i += 1) {
+    let koala = koalas[i];
+    // For each book, append a new row to our table
+    $('#viewKoalas').append(`
+      <tr>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.ready}</td>
+        <td>${koala.notes}</td>
+      </tr>
+    `);
+  }
+}
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
